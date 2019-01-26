2ea4b1bdcaad397a1e917bab9e3256facf14093f
OnderGrmz <ondergormez@gmail.com>
2019-01-26 17:12:59 +0300

    - Makefile lar duzenlendi. - Deneme projeleri eklendi.

---
+++ Project/02-src/main.cpp
@@ -0,0 +1,36 @@
+/**
+ * Copyright (C) 2018 Önder Görmez
+
+ * This program is free software: you can redistribute it and/or modify it under the terms of the GNU General
+ * Public License as published by the Free Software Foundation, either version 3 of the License, or (at your 
+ * option) any later version. This program is distributed in the hope that it will be useful, but WITHOUT ANY 
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the 
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License along with this program. If not, see 
+ * http://www.gnu.org/licenses/.
+
+ * For any questions, please contact me @ ondergormez@gmail.com.
+ */
+
+#include <iostream>
+#include "Utility.hpp"
+#include "Array.hpp"
+
+using namespace std;
+
+int main(void)
+{
+    cout << "This is a test project" << endl;
+    //Array *arrayOperations = new Array();
+    //Utility *utilityOperations = new Utility();   
+
+    Array::DisplayLibraryVersion();
+    Utility::DisplayLibraryVersion();
+
+    vector<uint32_t> testArray = vector<uint32_t>(7);
+    Array::FillRandomValue(testArray);
+    Utility::DisplayVector(testArray);
+
+    return 0;
+}
