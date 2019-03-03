4854f8842a00240b74dc1298238ff32a530a0469
OnderGrmz <ondergormez@gmail.com>
2019-03-04 01:24:34 +0300

    - Makefile ler kopyalanmadan once eskisi siliniyor.

--- CopyMakefileToFolders.pl
+++ CopyMakefileToFolders.pl
@@ -13,11 +13,22 @@ my $LIB_RANDOM_PATH = "libRandomArray/v1.0/";
 my $LIB_SORTING_PATH = "libSorting/v1.0/";
 my $LIB_UTILITY_PATH = "libUtility/v1.0/";
 
+printf("rm $LIB_RANDOM_PATH$MAKEFILE\n");
+system(sprintf("rm $LIB_RANDOM_PATH$MAKEFILE\n"));
+
 printf("cp $MAKEFILE $LIB_RANDOM_PATH\n");
 system(sprintf("cp $MAKEFILE $LIB_RANDOM_PATH\n"));
 
+
+printf("rm $LIB_SORTING_PATH$MAKEFILE\n");
+system(sprintf("rm $LIB_SORTING_PATH$MAKEFILE\n"));
+
 printf("cp $MAKEFILE $LIB_SORTING_PATH\n");
 system(sprintf("cp $MAKEFILE $LIB_SORTING_PATH\n"));
+
+
+printf("rm $LIB_UTILITY_PATH$MAKEFILE\n");
+system(sprintf("rm $LIB_UTILITY_PATH$MAKEFILE\n"));
 
 printf("cp $MAKEFILE $LIB_UTILITY_PATH\n");
 system(sprintf("cp $MAKEFILE $LIB_UTILITY_PATH\n"));
