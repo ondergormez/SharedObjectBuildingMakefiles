01a071603e42511d31cafd160c0a27a9b3e70fd1
Önder Görmez <ondergormez@gmail.com>
2019-10-13 13:26:11 +0300

    - rpath, LD_LIBRARY_PATH opsiyonlari icin conditional compiling islemleri per dosyasina ekleniyor.

--- 02-DynamicLinking/Build&Run.pl
+++ 02-DynamicLinking/Build&Run.pl
@@ -8,28 +8,57 @@ use warnings;
 
 system(sprintf("clear"));
 
-printf("rm -rf *.o *.elf\n\n");
-system(sprintf("rm -rf *.o *.elf"));
+# get script name
+my $ScriptName = $0;
+my $ArgumentCount = $#ARGV + 1;
+my $BuildForRpath = "build_for_rpath";
+my $BuildFor_LD_LIBRARY_PATH = "build_for_LD_LIBRARY_PATH";
 
-# This part builds the my_strcmp file.
+sub printUsage {
+    printf("Usage:\n");
+    printf("$ScriptName $BuildForRpath\n");
+    printf("$ScriptName $BuildFor_LD_LIBRARY_PATH\n");
+}
+
+if ($ArgumentCount != 1) {
+    printf("Wrong argument count: $ArgumentCount!\n");
+    printUsage();
+    exit;
+}
+elsif (($ARGV[0] ne $BuildForRpath) && ($ARGV[0] ne $BuildFor_LD_LIBRARY_PATH)) {
+    print "\"$ARGV[0]\" wrong argument!\n";
+    printUsage();
+    exit;
+}
+
+printf("rm -rf *.o *.so *.elf\n\n");
+system(sprintf("rm -rf *.o *.so *.elf"));
+
+# This part builds the my_strcmp.o file.
 printf("g++ -c -Wall -fPIC -std=c++11 my_strcmp.cpp -o my_strcmp.o\n");
 system(sprintf("g++ -c -Wall -fPIC -std=c++11 my_strcmp.cpp -o my_strcmp.o"));
 printf("my_strcmp.o file created!\n\n");
 
-# This part builds the my_strlen file.
+# This part builds the my_strlen.o file.
 printf("g++ -c -Wall -fPIC -std=c++11 my_strlen.cpp -o my_strlen.o\n");
 system(sprintf("g++ -c -Wall -fPIC -std=c++11 my_strlen.cpp -o my_strlen.o"));
 printf("my_strlen.o file created!\n\n");
 
-# This part create shared library file.
+# This part create libMyStringOp.so shared library file.
 printf("g++ -shared -o libMyStringOp.so my_strcmp.o my_strlen.o\n");
 system(sprintf("g++ -shared -o libMyStringOp.so my_strcmp.o my_strlen.o"));
 printf("libMyStringOp.so library file created!\n\n");
 
-# This part builds the program with the required .o files.
-printf("g++ -Wall -std=c++11 main.cpp -L./ -lMyStringOp -o prog.elf\n");
-system(sprintf("g++ -Wall -std=c++11 main.cpp -L./ -lMyStringOp -o prog.elf"));
-printf("prog.elf file created!\n\n");
+if ($ARGV[0] eq $BuildForRpath) {
+
+}
+elsif ($ARGV[0] eq $BuildFor_LD_LIBRARY_PATH) {
+    # This part builds the program with the required .o files.
+    printf("g++ -Wall -std=c++11 main.cpp -L./ -lMyStringOp -o prog.elf\n");
+    system(sprintf("g++ -Wall -std=c++11 main.cpp -L./ -lMyStringOp -o prog.elf"));
+}
+    printf("prog.elf file created!\n\n");
 
 printf("./prog.elf\n");
 system(sprintf("./prog.elf"));
+printf("\n");
